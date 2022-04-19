67a43881da0568a96d507eabbe7cb095388061f2
grudovv <mariobg141@abv.bg>
2022-04-19 10:50:30 +0300

    Add project files.

---
+++ Views/LogReg.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace TheBlechleyCodeBraker.Views
+{
+    partial class LogReg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblRegister = new System.Windows.Forms.Label();
+            this.lblRegUsername = new System.Windows.Forms.Label();
+            this.lblRegPassword2 = new System.Windows.Forms.Label();
+            this.lblRegPassword = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtRegUsername = new System.Windows.Forms.TextBox();
+            this.txtRegPassword = new System.Windows.Forms.TextBox();
+            this.txtRegPassword2 = new System.Windows.Forms.TextBox();
+            this.btnLOGIN = new System.Windows.Forms.Button();
+            this.btnREGISTER = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(83, 25);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(200, 75);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "Login";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(12, 132);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(102, 30);
+            this.lblUsername.TabIndex = 1;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(13, 175);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(102, 30);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblRegister
+            // 
+            this.lblRegister.AutoSize = true;
+            this.lblRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegister.Location = new System.Drawing.Point(433, 27);
+            this.lblRegister.Name = "lblRegister";
+            this.lblRegister.Size = new System.Drawing.Size(271, 73);
+            this.lblRegister.TabIndex = 3;
+            this.lblRegister.Text = "Register";
+            // 
+            // lblRegUsername
+            // 
+            this.lblRegUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegUsername.Location = new System.Drawing.Point(378, 132);
+            this.lblRegUsername.Name = "lblRegUsername";
+            this.lblRegUsername.Size = new System.Drawing.Size(102, 30);
+            this.lblRegUsername.TabIndex = 4;
+            this.lblRegUsername.Text = "Username:";
+            // 
+            // lblRegPassword2
+            // 
+            this.lblRegPassword2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegPassword2.Location = new System.Drawing.Point(378, 217);
+            this.lblRegPassword2.Name = "lblRegPassword2";
+            this.lblRegPassword2.Size = new System.Drawing.Size(102, 30);
+            this.lblRegPassword2.TabIndex = 5;
+            this.lblRegPassword2.Text = "Password:";
+            // 
+            // lblRegPassword
+            // 
+            this.lblRegPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegPassword.Location = new System.Drawing.Point(378, 175);
+            this.lblRegPassword.Name = "lblRegPassword";
+            this.lblRegPassword.Size = new System.Drawing.Size(102, 30);
+            this.lblRegPassword.TabIndex = 6;
+            this.lblRegPassword.Text = "Password:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(133, 132);
+            this.txtUsername.Multiline = true;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(150, 30);
+            this.txtUsername.TabIndex = 7;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(133, 175);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(150, 30);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // txtRegUsername
+            // 
+            this.txtRegUsername.Location = new System.Drawing.Point(499, 132);
+            this.txtRegUsername.Multiline = true;
+            this.txtRegUsername.Name = "txtRegUsername";
+            this.txtRegUsername.Size = new System.Drawing.Size(150, 30);
+            this.txtRegUsername.TabIndex = 9;
+            // 
+            // txtRegPassword
+            // 
+            this.txtRegPassword.Location = new System.Drawing.Point(499, 175);
+            this.txtRegPassword.Multiline = true;
+            this.txtRegPassword.Name = "txtRegPassword";
+            this.txtRegPassword.Size = new System.Drawing.Size(150, 30);
+            this.txtRegPassword.TabIndex = 10;
+            // 
+            // txtRegPassword2
+            // 
+            this.txtRegPassword2.Location = new System.Drawing.Point(499, 217);
+            this.txtRegPassword2.Multiline = true;
+            this.txtRegPassword2.Name = "txtRegPassword2";
+            this.txtRegPassword2.Size = new System.Drawing.Size(150, 30);
+            this.txtRegPassword2.TabIndex = 11;
+            // 
+            // btnLOGIN
+            // 
+            this.btnLOGIN.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLOGIN.Location = new System.Drawing.Point(16, 304);
+            this.btnLOGIN.Name = "btnLOGIN";
+            this.btnLOGIN.Size = new System.Drawing.Size(267, 103);
+            this.btnLOGIN.TabIndex = 12;
+            this.btnLOGIN.Text = "LOGIN";
+            this.btnLOGIN.UseVisualStyleBackColor = true;
+            // 
+            // btnREGISTER
+            // 
+            this.btnREGISTER.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnREGISTER.Location = new System.Drawing.Point(372, 304);
+            this.btnREGISTER.Name = "btnREGISTER";
+            this.btnREGISTER.Size = new System.Drawing.Size(386, 103);
+            this.btnREGISTER.TabIndex = 13;
+            this.btnREGISTER.Text = "REGISTER";
+            this.btnREGISTER.UseVisualStyleBackColor = true;
+            // 
+            // LogReg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 450);
+            this.Controls.Add(this.btnREGISTER);
+            this.Controls.Add(this.btnLOGIN);
+            this.Controls.Add(this.txtRegPassword2);
+            this.Controls.Add(this.txtRegPassword);
+            this.Controls.Add(this.txtRegUsername);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblRegPassword);
+            this.Controls.Add(this.lblRegPassword2);
+            this.Controls.Add(this.lblRegUsername);
+            this.Controls.Add(this.lblRegister);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblLogin);
+            this.Name = "LogReg";
+            this.Text = "LogReg";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblRegister;
+        private System.Windows.Forms.Label lblRegUsername;
+        private System.Windows.Forms.Label lblRegPassword2;
+        private System.Windows.Forms.Label lblRegPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtRegUsername;
+        private System.Windows.Forms.TextBox txtRegPassword;
+        private System.Windows.Forms.TextBox txtRegPassword2;
+        private System.Windows.Forms.Button btnLOGIN;
+        private System.Windows.Forms.Button btnREGISTER;
+    }
+}
