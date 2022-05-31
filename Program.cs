7ee31010f4753b9460598f9d8fa07acb0748992c
it.pgee.burgas <it.pgee.burgas@gmail.com>
2022-05-31 10:41:26 +0300

    Database_V2

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace TheBlechleyCodeBraker
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LogReg());
-            // zzzzzz
+            // zzzzzzx
         }
     }
 }
