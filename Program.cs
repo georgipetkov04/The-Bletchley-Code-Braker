3b37401b9b3b5ac64d80843e227a58e77207aa82
it.pgee.burgas <it.pgee.burgas@gmail.com>
2022-05-31 10:29:39 +0300

    DataBase created 1' '

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace TheBlechleyCodeBraker
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LogReg());
+            // zzzzzz
         }
     }
 }
